ece3484407cf67180e2088188f228b57c094f164
S3-20181650 <39684697+S3-20181650@users.noreply.github.com>
2021-07-01 17:56:48 +0900

    coommit

--- TranslationPage/src/app/app.component.ts
+++ TranslationPage/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { Projectdata } from './projectdata';
+import { HttpClient } from '@angular/common/http';
+import { DataService } from './data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TranslationPage';
+  data$: Projectdata[] = []; // import ProjectData (data.serveice.ts)
+  // import ProjectData (data.serveice.ts)
+  // postData: File  //type이 File인 변수 postData
+  
+  // url = `http://192.168.0.50:8000/api/expert/upload/`;  //post 보낼 url 주소
+  // json;
+  // options = {headers:`multipart/form-data; boundary=<calculated when request is sent>`};
+
+  /*post 인자값으로 url이랑 postData를 넘겨줌. 뒤에 promise 구문은 확실하게 모르겠어서,,ㅜ 아마 url에 있는 
+  정보를 찍어보는 용도이지 않을까 싶네*/
+  constructor(private dataService: DataService, private http: HttpClient){
+    // this.http.post(this.url, this.postData, {headers: this.options}).toPromise().then((data:any) =>{
+    //   console.log(data);
+    //   this.json = JSON.stringify(data.json);
+    // });
+  }
+
+  ngOnInit(){
+    return this.dataService.getUsers()
+      .subscribe(data => {this.data$ = data;
+      console.log(this.data$);
+      })
+  }
 }
---
+++ TranslationPage/src/app/data.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Projectdata } from './projectdata';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  restApiUrl='http://192.168.0.50:8000/api/expert/project/' //project data 있는 서버 주소
+  constructor(private _http:HttpClient ) { }
+  getUsers(){
+    return this._http.get<Projectdata[]>(this.restApiUrl)
+  }
+}
